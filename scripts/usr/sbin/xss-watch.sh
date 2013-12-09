454f754a8afb39584025d9ddbfd6caf4463e8bc5
inhies <inhies@gmail.com>
2013-12-09 11:38:14 -1000

    Screensaver tweaks

--- scripts/usr/sbin/xss-watch.sh
+++ scripts/usr/sbin/xss-watch.sh
@@ -4,12 +4,13 @@ process() {
 	while read input; do 
 		case "$input" in
 		UNBLANK*)	
-			#/usr/bin/purple-remote "setstatus?status=available&message=" 
+			/usr/bin/purple-remote "setstatus?status=available&message=" 
 			notify-send DUNST_COMMAND_RESUME
 			;;
 		LOCK*)	
 			notify-send DUNST_COMMAND_PAUSE
-			#/usr/bin/purple-remote "setstatus?status=away&message=" 
+			/usr/bin/purple-remote "setstatus?status=away&message=" 
+			keepass --lock-all
 			;;
 		esac
 	done
